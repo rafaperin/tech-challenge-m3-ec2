fa85752aae7e72a4c5249f1bd20fa1fd4368f154
Rafael <rafaelpfer@hotmail.com>
2023-11-04 18:10:16 -0300

    Adjusted terraform variables name

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "aws_access_key_id" {
+variable "tf_var_aws_access_key_id" {
   description = "Access key ID from AWS IAM"
   type        = string
   sensitive   = true
 }
-variable "aws_secret_acess_key" {
+variable "tf_var_aws_secret_acess_key" {
   description = "Secret access key from AWS IAM"
   type        = string
   sensitive   = true
